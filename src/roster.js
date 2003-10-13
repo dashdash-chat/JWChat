2189d62c2c18de68e1a6f2454d74c2d8f714ab9d
Stefan Strigler <stefan@strigler.de>
2003-10-13 15:00:26 +0000

    added a comment

--- src/roster.js
+++ src/roster.js
@@ -43,7 +43,7 @@ function RosterUser(jid,subscription,groups,name) {
     this.name = name;
   else if (this.jid == JABBERSERVER)
     this.name = loc("System");
-  else if (this.jid.indexOf('@') != -1 && this.jid.substring(this.jid.indexOf('@')+1) == JABBERSERVER)
+  else if ((this.jid.indexOf('@') != -1) && this.jid.substring(this.jid.indexOf('@')+1) == JABBERSERVER) // we found a local user
     this.name = this.jid.substring(0,jid.indexOf('@'));
   else
     this.name = this.jid;
