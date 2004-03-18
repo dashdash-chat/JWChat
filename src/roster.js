34646f6ca2064a8993110e92cd9dba5da1997110
Stefan Strigler <stefan@strigler.de>
2004-03-18 12:55:00 +0000

    excape all params to open-url code cleanup

--- src/roster.js
+++ src/roster.js
@@ -23,10 +23,10 @@ var grp_close_img = new Image();
 grp_close_img.src = 'images/group_close.gif';
 
 function RosterGroup(name) {
-	this.name = name;
-	this.users = new Array();
-	this.onlUserCount = 0;
-	this.messagesPending = 0;
+  this.name = name;
+  this.users = new Array();
+  this.onlUserCount = 0;
+  this.messagesPending = 0;
 }
 
 function RosterUserAdd2Group(group) {
@@ -34,9 +34,9 @@ function RosterUserAdd2Group(group) {
 }
 
 function RosterUser(jid,subscription,groups,name) {
-	this.jid = jid || 'unknown';
+  this.jid = jid || 'unknown';
 
-	this.subscription = subscription || 'none';
+  this.subscription = subscription || 'none';
   this.groups = groups || [''];
 
   if (name)
@@ -51,12 +51,12 @@ function RosterUser(jid,subscription,groups,name) {
   this.name = htmlEnc(this.name);
 
   // initialise defaults
-	this.status = (this.subscription == 'from' || this.subscription == 'none') ? 'stalker' : 'unavailable';
-	this.statusMsg = null;
-	this.lastsrc = null;
-	this.messages = new Array();
-	this.chatmsgs = new Array();
-	this.chatW = null; // chat window
+  this.status = (this.subscription == 'from' || this.subscription == 'none') ? 'stalker' : 'unavailable';
+  this.statusMsg = null;
+  this.lastsrc = null;
+  this.messages = new Array();
+  this.chatmsgs = new Array();
+  this.chatW = null; // chat window
 
   // methods
   this.add2Group = RosterUserAdd2Group;
@@ -64,19 +64,19 @@ function RosterUser(jid,subscription,groups,name) {
 }
 
 function getElFromArrByProp(arr,prop,str) {
-	for (var i in arr) {
-		if (arr[i][prop] == str)
-			return arr[i];
-	}
-	return null;
+  for (var i in arr) {
+    if (arr[i][prop] == str)
+      return arr[i];
+  }
+  return null;
 }
 
 function getRosterGroupByName(groupName) {
-	return getElFromArrByProp(this.groups,"name",groupName);
+  return getElFromArrByProp(this.groups,"name",groupName);
 }
 
 function getRosterUserByJID(jid) {
-	return getElFromArrByProp(this.users,"jid",jid);
+  return getElFromArrByProp(this.users,"jid",jid);
 }
 
 function RosterUpdateStyleIE() {
@@ -129,9 +129,9 @@ function RosterOpenMessage(jid) {
   }
 
   if (user.messages.length > 0 && (!user.mW || user.mW.closed)) // display messages
-    user.mW = open('message.html?jid='+jid,"mw"+wName,'width=320,height=230,dependent=yes,resizable=yes');
+    user.mW = open('message.html?jid='+escape(jid),"mw"+wName,'width=320,height=230,dependent=yes,resizable=yes');
   else if (!user.sW || user.sW.closed) // open send dialog
-    user.sW = open("send.html?jid="+jid,"sw"+wName,'width=320,height=200,dependent=yes,resizable=yes');
+    user.sW = open("send.html?jid="+escape(jid),"sw"+wName,'width=320,height=200,dependent=yes,resizable=yes');
   return false;
 }
 
@@ -139,7 +139,7 @@ function RosterOpenChat(jid) {
   var user = this.getUserByJID(jid);
   
   if (!user.chatW || user.chatW.closed)
-    user.chatW = open("chat.html?jid="+jid,"chatW"+makeWindowName(user.jid),"width=320,height=360,resizable=yes");
+    user.chatW = open("chat.html?jid="+escape(jid),"chatW"+makeWindowName(user.jid),"width=320,height=360,resizable=yes");
   else if (user.chatW.popMsgs)
     user.chatW.popMsgs();
 }
@@ -158,59 +158,59 @@ function RosterClose() {
 }
 
 function RosterUpdateGroupForUser(user) {
-	for (var j in user.groups) {
-		if (user.groups.length > 1 && user.groups[j] == '')
-			continue;
-		var groupName = (user.groups[j] == '') ? 'Unfiled' : user.groups[j];
-		var group = this.getGroupByName(groupName);
-		if(group == null) {
-			group = new RosterGroup(groupName);
-			this.groups = this.groups.concat(group);
-		}
-		group.users = group.users.concat(user);
-	}
+  for (var j in user.groups) {
+    if (user.groups.length > 1 && user.groups[j] == '')
+      continue;
+    var groupName = (user.groups[j] == '') ? 'Unfiled' : user.groups[j];
+    var group = this.getGroupByName(groupName);
+    if(group == null) {
+      group = new RosterGroup(groupName);
+      this.groups = this.groups.concat(group);
+    }
+    group.users = group.users.concat(user);
+  }
 }
 	
 function RosterUpdateGroups() {
-	this.groups = new Array();
-	for (var i in this.users)
-		this.updateGroupsForUser(this.users[i]);
+  this.groups = new Array();
+  for (var i in this.users)
+    this.updateGroupsForUser(this.users[i]);
 }
 
 function RosterUserAdd(user) {
-	this.users = this.users.concat(user);
+  this.users = this.users.concat(user);
 	
-	// add to groups
-	this.updateGroupsForUser(user);
-	return user;
+  // add to groups
+  this.updateGroupsForUser(user);
+  return user;
 }
 	
 function RosterRemoveUser(user) {
-	this.groups = new Array();
-	for (var i in this.users) {
-		if (user == this.users[i]) {
-			this.users = this.users.slice(0,i).concat(this.users.slice(i+1,this.users.length));
-			break;
-		}
-	}
-	this.updateGroups();
+  this.groups = new Array();
+  for (var i in this.users) {
+    if (user == this.users[i]) {
+      this.users = this.users.slice(0,i).concat(this.users.slice(i+1,this.users.length));
+      break;
+    }
+  }
+  this.updateGroups();
 }
 	
 function Roster(items,targetW) {
-	this.users = new Array();
-	this.groups = new Array();
+  this.users = new Array();
+  this.groups = new Array();
   this.name = 'Roster';
 
   this.rosterW = targetW;
 	
-	/* object's methods */
-	this.print = printRoster;
-	this.getGroupByName = getRosterGroupByName;
-	this.getUserByJID = getRosterUserByJID;
-	this.addUser = RosterUserAdd;
-	this.removeUser = RosterRemoveUser;
-	this.updateGroupsForUser = RosterUpdateGroupForUser;
-	this.updateGroups = RosterUpdateGroups;
+  /* object's methods */
+  this.print = printRoster;
+  this.getGroupByName = getRosterGroupByName;
+  this.getUserByJID = getRosterUserByJID;
+  this.addUser = RosterUserAdd;
+  this.removeUser = RosterRemoveUser;
+  this.updateGroupsForUser = RosterUpdateGroupForUser;
+  this.updateGroups = RosterUpdateGroups;
   this.toggleGrp = RosterToggleGrp;
   this.updateStyleIE = RosterUpdateStyleIE;
   this.toggleHide = RosterToggleHide;
@@ -219,70 +219,70 @@ function Roster(items,targetW) {
   this.openChat = RosterOpenChat;
   this.close = RosterClose;
  
-	/* setup groups */
-	for (var i in items) {
-		/* if (items[i].jid.indexOf("@") == -1) */ // no user - must be a transport
-		if (typeof(items[i].jid) == 'undefined')
-			continue;
-		items[i].jid = cutResource(items[i].jid);
-		items[i].name = (items[i].name) ? items[i].name : items[i].jid;
-		this.addUser(new RosterUser(items[i].jid,items[i].subscription,items[i].groups,items[i].name));
-	}
+  /* setup groups */
+  for (var i in items) {
+    /* if (items[i].jid.indexOf("@") == -1) */ // no user - must be a transport
+    if (typeof(items[i].jid) == 'undefined')
+      continue;
+    items[i].jid = cutResource(items[i].jid);
+    items[i].name = (items[i].name) ? items[i].name : items[i].jid;
+    this.addUser(new RosterUser(items[i].jid,items[i].subscription,items[i].groups,items[i].name));
+  }
 }
 
 function rosterSort(a,b) {
-	return (a.name.toLowerCase()<b.name.toLowerCase())?-1:1;
+  return (a.name.toLowerCase()<b.name.toLowerCase())?-1:1;
 }
 
 function printRoster() {
 	
-	/* update user count for groups */
-	for (var i=0; i<this.groups.length; i++) {
-		this.groups[i].onlUserCount = 0;
-		this.groups[i].messagesPending = 0;
-		for (var j=0; j<this.groups[i].users.length; j++) {
-			if (this.groups[i].users[j].status != 'unavailable' && this.groups[i].users[j].status != 'stalker')
-				this.groups[i].onlUserCount++;
-			if (this.groups[i].users[j].lastsrc)
-				this.groups[i].messagesPending++;
-		}
-	}
+  /* update user count for groups */
+  for (var i=0; i<this.groups.length; i++) {
+    this.groups[i].onlUserCount = 0;
+    this.groups[i].messagesPending = 0;
+    for (var j=0; j<this.groups[i].users.length; j++) {
+      if (this.groups[i].users[j].status != 'unavailable' && this.groups[i].users[j].status != 'stalker')
+        this.groups[i].onlUserCount++;
+      if (this.groups[i].users[j].lastsrc)
+        this.groups[i].messagesPending++;
+    }
+  }
 	
-	var rosterHTML = '';
-
-	this.groups = this.groups.sort(rosterSort);
-	
-	for (var i=0; i<this.groups.length; i++) {
-		var rosterGroupHeadClass = (this.usersHidden && this.groups[i].onlUserCount == 0 && this.groups[i].messagesPending == 0) ? 'rosterGroupHeaderHidden':'rosterGroupHeader';
-		rosterHTML += "<div id='"+this.groups[i].name+"Head' class='"+rosterGroupHeadClass+"' onDblClick='toggleGrp(\""+this.groups[i].name+"\");'><nobr>";
-		var toggleImg = (this.groups[i].toggled)?'images/group_close.gif':'images/group_open.gif';
-		rosterHTML += "<img src='"+toggleImg+"' name='"+this.groups[i].name+"Img' align='middle' onClick='toggleGrp(\""+this.groups[i].name+"\");'> ";
-		rosterHTML += this.groups[i].name+ " (<span id='"+this.groups[i].name+"On'>"+this.groups[i].onlUserCount+"</span>/" + this.groups[i].users.length + ")";
-		rosterHTML += "</nobr></div>";
-		var rosterGroupClass = ((this.usersHidden && this.groups[i].onlUserCount == 0 && this.groups[i].messagesPending == 0) || this.groups[i].toggled)?'hidden':'rosterGroup';
-		rosterHTML += "<div id='"+this.groups[i].name+"' class='"+rosterGroupClass+"'>";
-		
-		this.groups[i].users = this.groups[i].users.sort(rosterSort);
-		for (var j=0; j<this.groups[i].users.length; j++) {
-			var user = this.groups[i].users[j];
-			var rosterUserClass = (this.usersHidden && (user.status == 'unavailable' || user.status == 'stalker') && !user.lastsrc) ? "hidden":"rosterUser";
-			rosterHTML += "<div id='"+user.jid+"/"+this.groups[i].name+"Entry' class='"+rosterUserClass+"' onClick=\"return userClicked(this,'"+user.jid+"');\"";
-			if (user.statusMsg)
-				rosterHTML += " title=\"" + user.statusMsg + "\"";
-			rosterHTML += "><nobr>";
-
-			var userImg = (user.lastsrc) ? messageImg : eval(user.status + "Led");
-			rosterHTML += "<img src='"+userImg.src+"' name='"+user.jid+"/"+this.groups[i].name+"' align='left'>";
-			rosterHTML += "<div><span class=\"nickName\">"+user.name+"</span>";
-			if (user.statusMsg)
+  var rosterHTML = '';
+  
+  this.groups = this.groups.sort(rosterSort);
+  
+  for (var i=0; i<this.groups.length; i++) {
+    var rosterGroupHeadClass = (this.usersHidden && this.groups[i].onlUserCount == 0 && this.groups[i].messagesPending == 0) ? 'rosterGroupHeaderHidden':'rosterGroupHeader';
+    rosterHTML += "<div id='"+this.groups[i].name+"Head' class='"+rosterGroupHeadClass+"' onDblClick='toggleGrp(\""+this.groups[i].name+"\");'><nobr>";
+    var toggleImg = (this.groups[i].toggled)?'images/group_close.gif':'images/group_open.gif';
+    rosterHTML += "<img src='"+toggleImg+"' name='"+this.groups[i].name+"Img' align='middle' onClick='toggleGrp(\""+this.groups[i].name+"\");'> ";
+    rosterHTML += this.groups[i].name+ " (<span id='"+this.groups[i].name+"On'>"+this.groups[i].onlUserCount+"</span>/" + this.groups[i].users.length + ")";
+    rosterHTML += "</nobr></div>";
+    var rosterGroupClass = ((this.usersHidden && this.groups[i].onlUserCount == 0 && this.groups[i].messagesPending == 0) || this.groups[i].toggled)?'hidden':'rosterGroup';
+    rosterHTML += "<div id='"+this.groups[i].name+"' class='"+rosterGroupClass+"'>";
+    
+    this.groups[i].users = this.groups[i].users.sort(rosterSort);
+    for (var j=0; j<this.groups[i].users.length; j++) {
+      var user = this.groups[i].users[j];
+      var rosterUserClass = (this.usersHidden && (user.status == 'unavailable' || user.status == 'stalker') && !user.lastsrc) ? "hidden":"rosterUser";
+      rosterHTML += "<div id='"+user.jid+"/"+this.groups[i].name+"Entry' class='"+rosterUserClass+"' onClick=\"return userClicked(this,'"+user.jid+"');\"";
+      if (user.statusMsg)
+        rosterHTML += " title=\"" + user.statusMsg + "\"";
+      rosterHTML += "><nobr>";
+      
+      var userImg = (user.lastsrc) ? messageImg : eval(user.status + "Led");
+      rosterHTML += "<img src='"+userImg.src+"' name='"+user.jid+"/"+this.groups[i].name+"' align='left'>";
+      rosterHTML += "<div><span class=\"nickName\">"+user.name+"</span>";
+      if (user.statusMsg)
         rosterHTML += "<br><nobr><span class=\"statusMsg\">"+user.statusMsg+"</span></nobr>";
       rosterHTML += "</div></nobr></div>";
-		}
-		rosterHTML += "</div>";
-	}
+    }
+    rosterHTML += "</div>";
+  }
 
-	this.rosterW.getElementById("roster").innerHTML = rosterHTML;
-	this.updateStyleIE();
+  this.rosterW.getElementById("roster").innerHTML = rosterHTML;
+  this.updateStyleIE();
 
   if (typeof(this.eprint) != 'undefined')
     this.eprint();
