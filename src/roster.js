576907f56603a6c87248f2f78ac1faa2d3c19c1a
Stefan Strigler <stefan@strigler.de>
2004-03-19 13:39:00 +0000

    put some space under status leds

--- src/roster.js
+++ src/roster.js
@@ -272,7 +272,7 @@ function printRoster() {
       rosterHTML += "><nobr>";
       
       var userImg = (user.lastsrc) ? messageImg : eval(user.status + "Led");
-      rosterHTML += "<img src='"+userImg.src+"' name='"+user.jid+"/"+this.groups[i].name+"' align='left'>";
+      rosterHTML += "<img src='"+userImg.src+"' name='"+user.jid+"/"+this.groups[i].name+"' align='left' style=\"padding-bottom: 4px;\">";
       rosterHTML += "<div><span class=\"nickName\">"+user.name+"</span>";
       if (user.statusMsg)
         rosterHTML += "<br><nobr><span class=\"statusMsg\">"+user.statusMsg+"</span></nobr>";
