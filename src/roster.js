9847cafd15b632a00b1647cfecccc83e3dc41353
Stefan Strigler <stefan@strigler.de>
2004-06-18 16:13:20 +0000

    set size of roster icons

--- src/roster.js
+++ src/roster.js
@@ -278,7 +278,7 @@ function printRoster() {
       rosterHTML += "><nobr>";
       
       var userImg = (user.lastsrc) ? messageImg : eval(user.status + "Led");
-      rosterHTML += "<img src='"+userImg.src+"' name='"+user.jid+"/"+this.groups[i].name+"' align='left'>";
+      rosterHTML += "<img src='"+userImg.src+"' name='"+user.jid+"/"+this.groups[i].name+"' width=16 height=16 border=0 align='left'>";
       rosterHTML += "<div><span class=\"nickName\">"+user.name+"</span>";
       if (user.statusMsg)
         rosterHTML += "<br clear=all><nobr><span class=\"statusMsg\">"+user.statusMsg+"</span></nobr>";
