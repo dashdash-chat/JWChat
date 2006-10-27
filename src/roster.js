86d8e55c0073c8290b813cbd98c06a968bd82a92
sdouglass <sdouglass>
2006-10-27 18:32:15 +0000

    add code to dynamically update the roster dom when roster users are added, modified, or removed (only for primary roster for now, not yet for group rosters); this will greatly improve performance over completely redrawing the roster every time, which is particularly important for very large rosters

--- src/roster.js
+++ src/roster.js
@@ -154,9 +154,27 @@ function RosterUpdateGroupForUser(user) {
 }
 	
 function RosterUpdateGroups() {
+  var oldGroups = this.groups;
   this.groups = new Array();
   for (var i=0; i<this.users.length; i++)
     this.updateGroupsForUser(this.users[i]);
+  // remove elements for groups that don't exist anymore
+  for (var i=0; i<oldGroups.length; i++) {
+    var remove = true;
+    for (var j=0; j<this.groups.length; j++) {
+      if (oldGroups[i].name == this.groups[j].name) {
+        remove = false;
+        break;
+}
+    }
+    if (remove) {
+      var rosterEl = this.rosterW.getElementById('roster');
+      var groupHeaderEl = this.rosterW.getElementById(oldGroups[i].name+'Head');
+      var groupEl = this.rosterW.getElementById(oldGroups[i].name);
+      rosterEl.removeChild(groupHeaderEl);
+      rosterEl.removeChild(groupEl);
+    }
+  }
 }
 
 function RosterUserAdd(user) {
@@ -172,6 +190,13 @@ function RosterRemoveUser(user) {
   for (var i=0; i<uLen; i++) {
     if (user == this.users[i]) {
       this.users = this.users.slice(0,i).concat(this.users.slice(i+1,uLen));
+      // remove user element
+      for (var j=0; j < user.groups.length; j++) {
+        var groupName = user.groups[j].name || 'Unfiled';
+        var groupEl = this.rosterW.getElementById(groupName);
+        var userEl = this.rosterW.getElementById(getUserElementId(user, user.groups[j]));
+        if (groupEl && userEl) { groupEl.removeChild(userEl); }
+      }
       break;
     }
   }
@@ -193,9 +218,11 @@ function Roster(items,targetW) {
   this.name = 'Roster';
 
   this.rosterW = targetW;
-	
+  this.lastUserSelected = null; // moved into Roster from iRoster.html and groupchat_iroster.html - sam
+
   /* object's methods */
   this.print = printRoster;
+  this.update = updateRoster;
   this.getGroupByName = getRosterGroupByName;
   this.getUserByJID = getRosterUserByJID;
   this.addUser = RosterUserAdd;
@@ -210,7 +237,9 @@ function Roster(items,targetW) {
   this.openChat = RosterOpenChat;
   this.cleanUp = RosterCleanUp;
   this.getGroupchats = RosterGetGroupchats;
- 
+  this.selectUser = RosterSelectUser; // moved into Roster from iRoster.html and groupchat_iroster.html - sam
+  this.userClicked = RosterUserClicked;
+
   /* setup groups */
   if (!items)
     return;
@@ -249,6 +278,11 @@ function printRoster() {
 
   this.groups = this.groups.sort(rosterSort);
 
+  if (this.rosterW.getElementById('roster').innerHTML.search(/\S/) > -1) {
+    this.update(); // update dom, rather than redrawing roster
+    return;
+  }
+
   var A = new Array();
 
 	/* ***
@@ -279,9 +313,11 @@ function printRoster() {
 		A[A.length] = this.groups[i].name;
 		A[A.length] = "On'>";
 		A[A.length] = this.groups[i].onlUserCount;
-		A[A.length] = "</span>/";
+		A[A.length] = "</span>/<span id='";// put total number in span - sam
+    A[A.length] = this.groups[i].name;
+    A[A.length] = "All'>";
 		A[A.length] = this.groups[i].users.length;
-		A[A.length] = ")";
+		A[A.length] = "</span>)";
     A[A.length] = "</nobr></div>";
     var rosterGroupClass = (
 			    (this.usersHidden && this.groups[i].onlUserCount == 0 && 
@@ -369,6 +405,234 @@ function printRoster() {
   this.updateStyleIE();
 }
 
+function getUserElementId(user, group) {
+  var groupName = group.name || 'Unfiled';
+  return htmlEnc(user.jid)+"/"+groupName+"Entry";
+}
+
+function getRosterUserClass(usersHidden, user, group) {
+  return (usersHidden && (user.status == 'unavailable' || user.status == 'stalker') && !user.lastsrc && group.name != loc("Gateways") ?
+          "hidden" : "rosterUser");
+}
+
+function getRosterGroupHeaderClass(usersHidden, group) {
+  return (usersHidden && group.onlUserCount == 0 && group.messagesPending == 0 && group.name != loc("Gateways") ?
+         'rosterGroupHeaderHidden':'rosterGroupHeader');
+}
+
+function getUserElementTitle(user) {
+  var elTitle = user.name
+  if (user.realjid) {
+    elTitle += "&#10;JID: ";
+    elTitle += htmlEnc(user.realjid);
+  } else {
+    elTitle += "&#10;JID: ";
+    elTitle += htmlEnc(user.jid);
+  }
+  elTitle += "&#10;";
+  elTitle += loc("Status");
+  elTitle += ": ";
+  elTitle += user.status;
+  if (user.statusMsg) {
+    elTitle += "&#10;";
+    elTitle += loc("Message");
+    elTitle += ": ";
+    elTitle += htmlEnc(user.statusMsg);
+  }
+  if ((user.messages.length + user.chatmsgs.length) > 0) {
+    elTitle += "&#10;";
+    elTitle += loc("[_1] message(s) pending",(user.messages.length + user.chatmsgs.length));
+  }
+  return elTitle;
+}
+
+function getUserInnerHTML(user, group) {
+  var userImg = (user.lastsrc) ? messageImg : eval(user.status + "Led");
+  var A = new Array();
+  A[A.length] = "<nobr><img src=\"";
+  A[A.length] = userImg.src;
+  A[A.length] = "\" name=\"";
+  A[A.length] = htmlEnc(user.jid);
+  A[A.length] = "/";
+  A[A.length] = group.name;
+  A[A.length] = "\" width='16' height='16' border='0' align='left'>";
+  A[A.length] = "<div><span class=\"nickName\">";
+  A[A.length] = user.name;
+  A[A.length] = "</span>";
+
+  if (user.statusMsg) {
+    A[A.length] = "<br clear=all><nobr><span class=\"statusMsg\">";
+    A[A.length] = htmlEnc(user.statusMsg);
+    A[A.length] = "</span></nobr>";
+  }
+  A[A.length] =  "</div></nobr>";
+  return A.join('');
+}
+
+function updateRoster() {
+  Debug.log('updating roster',2);
+  for (var i=0; i<this.groups.length; i++) {
+    var group = this.groups[i];
+    group.users = group.users.sort(rosterSort);
+    var groupEl = this.rosterW.getElementById(group.name);
+    if (groupEl) { // update group
+      for (var j=0; j<group.users.length; j++) {
+        var user = group.users[j];
+        var userElId = getUserElementId(user,group);
+        var userEl = this.rosterW.getElementById(userElId);
+        if (!userEl) { // add user
+          userEl = document.createElement('div');
+          userEl.id = userElId;
+          userEl.className = getRosterUserClass(this.usersHidden, user, group);
+          var onclickHandler = function(el, user, roster) {
+            var e = el;
+            var jid = htmlEnc(user.jid);
+            var r = roster;
+            var toggler = function() { r.userClicked(e, jid); };
+            return toggler;
+          }
+          userEl.onclick = onclickHandler(userEl, user, this);//"return userClicked(this,'"+htmlEnc(user.jid)+"');";
+          userEl.title = getUserElementTitle(user);
+          userEl.innerHTML = getUserInnerHTML(user, group);
+          var siblingEl;
+          var k = j+1;
+          while (!siblingEl && k<group.users.length) {
+            siblingEl = this.rosterW.getElementById(getUserElementId(group.users[k],group));
+            k++;
+          }
+          if (!siblingEl) {
+            groupEl.appendChild(userEl)
+          } else {
+            groupEl.insertBefore(userEl,siblingEl);
+          }
+        } else { // update user
+          userEl.className = getRosterUserClass(this.usersHidden, user, group);
+          userEl.title = getUserElementTitle(user);
+          var userImg = (user.lastsrc) ? messageImg : eval(user.status + "Led");
+          userEl.getElementsByTagName('img')[0].src = userImg.src;
+          var spanEls = userEl.getElementsByTagName('span');
+          spanEls[0].innerHTML = user.name
+          if (user.statusMsg) {
+            if (spanEls[1]) {
+              spanEls[1].innerHTML = htmlEnc(user.statusMsg);
+            } else {
+              var A = new Array();
+              A[A.length] = "<br clear=all><nobr><span class=\"statusMsg\">";
+              A[A.length] = htmlEnc(user.statusMsg);
+              A[A.length] = "</span></nobr>";
+              userEl.getElementsByTagName('div')[0].innerHTML += A.join('');
+            }
+          }
+        }
+      } // done with users
+      var groupHeaderEl = this.rosterW.getElementById(group.name+'Head');
+      if (groupHeaderEl) {
+        groupHeaderEl.className = getRosterGroupHeaderClass(this.usersHidden, group);
+        this.rosterW.getElementById(group.name+'On').innerHTML = group.onlUserCount;
+        this.rosterW.getElementById(group.name+'All').innerHTML = group.users.length;
+      }
+    } else { // add group
+      var groupHeaderEl = document.createElement('div');
+      groupHeaderEl.id = group.name+"Head";
+      groupHeaderEl.className = getRosterGroupHeaderClass(this.usersHidden, group);
+      var onclickHandler = function(group) {
+        var groupName = group.name;
+        var toggler = function() { parent.top.roster.toggleGrp(groupName); };
+        return toggler;
+      }
+      groupHeaderEl.onclick = onclickHandler(group);
+      var A = new Array();
+      A[A.length] = "<nobr>";
+      var toggleImg = (this.hiddenGroups[group.name])?'images/group_close.gif':'images/group_open.gif';
+      A[A.length] = "<img src='";
+      A[A.length] = toggleImg;
+      A[A.length] ="' name='";
+      A[A.length] = group.name;
+      A[A.length] = "Img'> ";
+      A[A.length] = group.name;
+      A[A.length] = " (<span id='";
+      A[A.length] = group.name;
+      A[A.length] = "On'>";
+      A[A.length] = group.onlUserCount;
+      A[A.length] = "</span>/<span id='";// put total number in span, also - sam
+      A[A.length] = group.name;
+      A[A.length] = "All'>";
+      A[A.length] = group.users.length;
+      A[A.length] = "</span>)";
+      A[A.length] = "</nobr>";
+      groupHeaderEl.innerHTML = A.join('');
+
+      groupEl = document.createElement('div');
+      groupEl.id = group.name;
+      var rosterGroupClass = (
+            (this.usersHidden && group.onlUserCount == 0 &&
+             group.messagesPending == 0 &&
+             group.name != loc("Gateways"))
+            || this.hiddenGroups[group.name])
+        ? 'hidden':'rosterGroup';
+      groupEl.className = rosterGroupClass;
+
+      A = new Array();
+      for (var j=0; j<group.users.length; j++) {
+        var user = group.users[j];
+
+        A[A.length] = "<div id=\"";
+        A[A.length] = htmlEnc(user.jid);
+        A[A.length] = "/";
+        A[A.length] = group.name;
+        A[A.length] = "Entry\" class=\"";
+        A[A.length] = getRosterUserClass(this.usersHidden, user, group);
+        A[A.length] = "\" onClick=\"return userClicked(this,'";
+        A[A.length] = htmlEnc(user.jid);
+        A[A.length] = "');\" title=\"";
+        A[A.length] = getUserElementTitle(user);
+        A[A.length] = "\">";
+        A[A.length] = getUserInnerHTML(user, group);
+        A[A.length] =  "</div>";
+      } /* END inner loop */
+      groupEl.innerHTML = A.join('');
+
+      var siblingEl;
+      var j = i + 1;
+      while (!siblingEl && j < this.groups.length) {
+        siblingEl = this.rosterW.getElementById(this.groups[j].name+'Head');
+        j++;
+      }
+      var rosterEl = this.rosterW.getElementById("roster");
+      if (!siblingEl) {
+        rosterEl.appendChild(groupHeaderEl)
+        rosterEl.appendChild(groupEl);
+      } else {
+        rosterEl.insertBefore(groupHeaderEl, siblingEl);
+        rosterEl.insertBefore(groupEl, siblingEl);
+      }
+    }
+  }
+}
+
+// moved into Roster from iRoster.html and groupchat_iroster.html - sam
+function RosterSelectUser(el) {
+  if(this.lastUserSelected)
+    this.lastUserSelected.className = "rosterUser";
+  el.className = "rosterUserSelected";
+  this.lastUserSelected = el;
+}
+
+function RosterUserClicked(el,jid) {
+  this.selectUser(el);
+
+  if (this.name == 'GroupchatRoster') {
+    return parent.top.user.roster.openChat(jid);
+  }
+
+  var user = parent.top.roster.getUserByJID(parent.top.cutResource(jid));
+
+	if(user && typeof(user.type) != 'undefined' && user.type == 'groupchat')
+		return parent.top.openGroupchat(jid);
+
+	if (!parent.top.isGateway(jid))
+		return parent.top.roster.openChat(jid);
+}
 
 /***********************************************************************
  * GROUPCHAT ROSTER
