dashdash-chat/JWChat

commit 00f25df7f645d4cd610fb60c3bcc07e0405b15cd

renamed JWC::I18N.pm

scripts/JWCI18N.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 
 
-package JWCI18N;
+package JWC::I18N;
 use base qw(Locale::Maketext);
 
 use Locale::Maketext 1.01;

commit 409847ab9500ee5b8e5f50ba2f9afc3744b16f69

Fix make errors
I have no idea why the make process was broken by 00f25df7f645d4cd610fb60c3bcc07e0405b15cd and then never fixed.

It seems like JSJaC was moved from svn.jwchat.org to GitHub - hopefully the newer version of the library doesn't break anything!

po/en.pm
@@ -1,5 +1,5 @@
-package JWC::I18N::en;
-use base qw(JWC::I18N);
+package JWCI18N::en;
+use base qw(JWCI18N);
 %Lexicon = (
 	    '_AUTO' => 1,
 	   );

po/en_us.pm
@@ -1,5 +1,5 @@
-package JWC::I18N::en_us;
-use base qw(JWC::I18N);
+package JWCI18N::en_us;
+use base qw(JWCI18N);
 %Lexicon = (
 	    '_AUTO' => 1,
 	   );

scripts/JWCI18N.pm
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 
 
-package JWC::I18N;
+package JWCI18N;
 use base qw(Locale::Maketext);
 
 use Locale::Maketext 1.01;

scripts/cplibs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ ! -e ./lib/jsjac ]; then
   echo "jsjac is missing, checking out ..."
-  svn co http://svn.jwchat.org/jsjac/trunk lib/jsjac
+  git clone git://github.com/sstrigler/JSJaC.git lib/jsjac
   echo "building jsjac ..."
   make -C lib/jsjac
   echo "done"